ffba74e56944a448941660778cc1d63747ad1948
a <a>
2021-09-14 13:44:25 +0200

    generate_new_states

--- robot_program/test.py
+++ robot_program/test.py
@@ -1,4 +1,4 @@
-class state:
+class State:
 	def __init__(self, cans, robot):
 		self._cans = cans
 		self._robot = robot
@@ -18,14 +18,14 @@ def board_translation(board):
 				robot = i * len(board[i]) + j
 			elif board[i][j] == '.':
 				new_line.append(0)
-				target.append(i * len(board[i]) + j)
+				target.append((i,j))
 			elif board[i][j] == '*':
 				new_line.append(0)
-				target.append(i * len(board[i]) + j)
-				cans.append(i * len(board[i]) + j)
+				target.append((i,j))
+				cans.append((i,j))
 			elif board[i][j] == '$':
 				new_line.append(0)
-				can.append(i * len(board[i]) + j)
+				can.append((i,j))
 			elif board[i][j] == ' ':
 				new_line.append(0)
 			else:
@@ -45,57 +45,58 @@ def check(list1, list2):
 def generate_new_states(state):
 	#up
 	ret = []
-	if state._robot > x:
-		if board[state._robot - x] == 0:
-			if state._robot - x in state._cans:
-				if board[state._robot - 2 * x] == 0 and state._robot - 2 * x not in state._cans and state._robot > 2 * x:
+	if state._robot[0] > 0:
+		if board[state._robot[0] - 1][state._robot[1]] == 0:
+			if (state._robot[0] - 1, state._robot[1]) in state._cans: 
+				if board[state._robot[0] - 2][state._robot[1]] == 0 and (state._robot[0] - 2, state._robot[1]) not in state._cans and state._robot[0] > 1:
 					new_cans = []
 					for i in state._cans:
-						if i != state._robot - x:
+						if i != (state._robot[0] - 1, state._robot[1]):
 							new_cans.append(i)
-					new_cans.append(state._robot - 2 * x)
-					ret.append(state(new_cans, state._robot - x))
+					new_cans.append((state._robot[0] - 2, state._robot[1]))
+					ret.append(State(new_cans, (state._robot[0] - 1, state._robot[1])))
 			else:
-				ret.append(state(state._cans, state._robot - x))
-	if state._robot < y - x:
-		if board[state._robot + x] == 0:
-			if state._robot + x in state._cans:
-				if board[state._robot + 2 * x] == 0 and state._robot + 2 * x not in state._cans and state._robot < y - 2 * x:
+				ret.append(State(state._cans, (state._robot[0] - 1, state._robot[1])))
+	if state._robot[0] < y:
+		if board[state._robot[0] + 1][state._robot[1]] == 0:
+			if (state._robot[0] + 1, state._robot[1]) in state._cans:
+				if board[state._robot[0] + 2][state._robot[1]] == 0 and (state._robot[0] + 2, state._robot[1]) not in state._cans and state._robot[0] < y - 1:
 					new_cans = []
 					for i in state._cans:
-						if i != state._robot + x:
+						if i != (state._robot[0] + 1, state._robot[1]):
 							new_cans.append(i)
-							new_cans.append(state._robot + 2 * x)
-					ret.append(state(new_cans, state._robot + x))
+						new_cans.append((state._robot[0] + 2, state._robot[1]))
+					ret.append(State(new_cans, (state._robot[0] + 1, state._robot[1])))
 			else:
-				ret.append(state(state._cans, state._robot + x))
-	if state._robot > 1:
-		if board[state._robot - 1] == 0:
-			if state._robot - 1 in state._cans:
-				if board[state._robot - 2] == 0 and state._robot - 2 not in state._cans and state._robot > 2:
+				ret.append(State(state._cans, (state._robot[0] + 1, state._robot[1])))
+	if state._robot[1] < x:
+		if board[state._robot[0]][state._robot[1] + 1] == 0:
+			if (state._robot[0], state._robot[1] + 1) in state._cans:
+				if board[state._robot[0]][state._robot[1] + 2] == 0 and (state._robot[0], state._robot[1] + 2) not in state._cans and state._robot[1] < x - 1:
 					new_cans = []
-					for i in states._cans:
-						if i != state._robot - 1:
+					for i in state._cans:
+						if i != (state._robot[0], state._robot[1] + 1):
 							new_cans.append(i)
-						new_cans.append(state._robot - 2)
-					ret.append(state(new_cans, state._robot - 1))
-				else:
-					ret.append(state(state._cans, state._robot - 1))
-	if state._robot < x - 1:
-		if board[state._robot + 1] == 0:
-			if state._robot + 1 in state._cans:
-				if board[state._robot + 2] == 0 and state._robot + 2 not in state._cans and state._robot < x - 2:
-					new_cans = []
-					for i in states._cans:
-						if i != state._robot + 1:
-							new_cans.append(i)
-						new_cans.append(state._robot + 2)
-					ret.append(state(new_cans, state._robot + 1))
+						new_cans.append((state._robot[0], state._robot[1] + 2))
+					ret.append(State(new_cans, (state._robot[0], state._robot[1] + 1)))
 			else:
-				ret.append(state(state._cans, state._robot + 1))
+				ret.append(State(state._cans, (state._robot[0], state._robot[1] + 1)))
+	if state._robot[1] > 0:
+		if board[state._robot[0]][state._robot[1] - 1] == 0:
+			if (state._robot[0], state._robot[1] - 1) in state._cans:
+				if board[state._robot[0]][state._robot[1] - 2] == 0 and (state._robot[0], state._robot[1] - 2) not in state._cans and state._robot[1] > 1:
+					new_cans = []
+					for i in state._cans:
+						if i != (state._robot[0], state._robot[1] - 1):
+							new_cans.append(i)
+						new_cans.append((state._robot[0], state._robot[1] - 2)) 
+					ret.append(State(new_cans, (state._robot[0], state._robot[1] - 1)))
+			else:
+				ret.append(State(state._cans, (state._robot[0], state._robot[1] - 1)))
 	return ret
+	
 
-target = [19,32,46]
+target = [(1,1),(1,2),(1,3)]
 board = [	[1,1,1,1,1,1,1,1,1],
 			[1,0,0,0,0,0,0,0,1],
 			[1,0,1,0,1,0,0,0,1],
@@ -106,9 +107,11 @@ board = [	[1,1,1,1,1,1,1,1,1],
 x = len(board[0])
 y = len(board)
 
-robot = 10
-cans = [21, 39, 46]
+robot = (5,3)
+cans = [(5,5), (5,6), (5,7)]
 
-a = state(cans, robot)
-print(generate_new_states(a))
+a = State(cans, robot)
+b = generate_new_states(a)
+for i in b:
+	print (i._robot)
 
