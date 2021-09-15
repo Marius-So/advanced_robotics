3f650879c16c112d4370723d4d095b66f2a9e2b9
a <a>
2021-09-15 22:58:36 +0200

    brute force :)

--- robot_program/test.py
+++ robot_program/test.py
@@ -1,9 +1,22 @@
 from copy import deepcopy
 
 class State:
-	def __init__(self, cans, robot):
+	def __init__(self, cans, robot, prev: int):
 		self._cans = cans
 		self._robot = robot
+		self._prev = prev
+
+	def __eq__(self, x):
+		if self._cans == x._cans and self._robot == x._robot:
+			return True
+		return False
+
+	def __str__(self):
+		a = ""
+		a += str(self._robot)
+		for i in self._cans:
+			a +=str(i)
+		return a
 
 def board_translation(board):
 	robot = -1
@@ -44,8 +57,7 @@ def check(list1, list2):
 			return False
 	return True
 
-def generate_new_states(state):
-	#up
+def generate_new_states(state, prev):
 	ret = []
 	if state._robot[0] > 0:
 		if board[state._robot[0] - 1][state._robot[1]] == 0:
@@ -56,9 +68,9 @@ def generate_new_states(state):
 						if i != (state._robot[0] - 1, state._robot[1]):
 							new_cans.append(i)
 					new_cans.append((state._robot[0] - 2, state._robot[1]))
-					ret.append(State(new_cans, (state._robot[0] - 1, state._robot[1])))
+					ret.append(State(new_cans, (state._robot[0] - 1, state._robot[1]),prev))
 			else:
-				ret.append(State(state._cans, (state._robot[0] - 1, state._robot[1])))
+				ret.append(State(state._cans, (state._robot[0] - 1, state._robot[1]),prev))
 	if state._robot[0] < y:
 		if board[state._robot[0] + 1][state._robot[1]] == 0:
 			if (state._robot[0] + 1, state._robot[1]) in state._cans:
@@ -67,10 +79,10 @@ def generate_new_states(state):
 					for i in state._cans:
 						if i != (state._robot[0] + 1, state._robot[1]):
 							new_cans.append(i)
-						new_cans.append((state._robot[0] + 2, state._robot[1]))
-					ret.append(State(new_cans, (state._robot[0] + 1, state._robot[1])))
+					new_cans.append((state._robot[0] + 2, state._robot[1]))
+					ret.append(State(new_cans, (state._robot[0] + 1, state._robot[1]),prev))
 			else:
-				ret.append(State(state._cans, (state._robot[0] + 1, state._robot[1])))
+				ret.append(State(state._cans, (state._robot[0] + 1, state._robot[1]),prev))
 	if state._robot[1] < x:
 		if board[state._robot[0]][state._robot[1] + 1] == 0:
 			if (state._robot[0], state._robot[1] + 1) in state._cans:
@@ -79,10 +91,10 @@ def generate_new_states(state):
 					for i in state._cans:
 						if i != (state._robot[0], state._robot[1] + 1):
 							new_cans.append(i)
-						new_cans.append((state._robot[0], state._robot[1] + 2))
-					ret.append(State(new_cans, (state._robot[0], state._robot[1] + 1)))
+					new_cans.append((state._robot[0], state._robot[1] + 2))
+					ret.append(State(new_cans, (state._robot[0], state._robot[1] + 1), prev))
 			else:
-				ret.append(State(state._cans, (state._robot[0], state._robot[1] + 1)))
+				ret.append(State(state._cans, (state._robot[0], state._robot[1] + 1), prev))
 	if state._robot[1] > 0:
 		if board[state._robot[0]][state._robot[1] - 1] == 0:
 			if (state._robot[0], state._robot[1] - 1) in state._cans:
@@ -91,12 +103,34 @@ def generate_new_states(state):
 					for i in state._cans:
 						if i != (state._robot[0], state._robot[1] - 1):
 							new_cans.append(i)
-						new_cans.append((state._robot[0], state._robot[1] - 2)) 
-					ret.append(State(new_cans, (state._robot[0], state._robot[1] - 1)))
+					new_cans.append((state._robot[0], state._robot[1] - 2)) 
+					ret.append(State(new_cans, (state._robot[0], state._robot[1] - 1), prev))
 			else:
-				ret.append(State(state._cans, (state._robot[0], state._robot[1] - 1)))
+				ret.append(State(state._cans, (state._robot[0], state._robot[1] - 1), prev))
 	return ret
 	
+
+def solve_sokoban(begining):
+    target.sort()
+    states_list = [begining]
+    begining = 0
+    end = 1
+    while (True):
+        a = 0
+        for i in range(begining, end):
+            new = generate_new_states(states_list[i], i)
+            for j in new:
+                if j not in states_list:
+                    #printStateFancy(j)
+                    states_list.append(j)
+                    j._cans.sort()
+                    a += 1
+                    if target == j._cans:
+                        return states_list
+        begining = end
+        end += a
+        if a == 0:
+            return states_list
 
 def check_board():
 	# check for target ion wall
@@ -104,18 +138,10 @@ def check_board():
 		if board[t[0]][t[1]] == "1":
 			print("there is a mistake here")
 
-def printState(state):
-	b = deepcopy(board)
-	for t in target:
-		b[t[0]][t[1]]="T"
-	for c in state._cans:
-		b[c[0]][c[1]]="C"
-	b[state._robot[0]][state._robot[1]]="R"			
-	for i in range(len(board)):
-		line = ""
-		for j in range(len(board[i])):
-			line = line + str(b[i][j]) + " "
-		print(line)
+def recursive_print_traject(l, i):
+	if l[i]._prev != -1:
+		recursive_print_traject(l, l[i]._prev)
+	printStateFancy(l[i])
 
 def printStateFancy(state):
 	for i in range(len(board)):
@@ -126,28 +152,17 @@ def printStateFancy(state):
 			if(e == 1): e = "⊠"  #Walls
 			for t in target:
 				if(t == (i,j)): e = "✛" #Target
-			for c in cans:
+			for c in state._cans:
 				if(c == (i,j)): e = "⬤" #Can
 			if ((state._robot[0] , state._robot[1]) == (i,j)): e = "△" #Can
 			line = line + str(e) + " "
 		print(line)
 
-def printCo():
-	print()
-	
 board = [	[1,1,1,1,1,1,1,1,1],
 			[1,0,0,0,0,0,0,0,1],
 			[1,0,0,0,0,0,0,0,1],
 			[1,0,0,1,1,1,0,0,1],
 			[1,0,0,0,0,0,0,0,1],
-			[1,0,0,0,0,0,0,0,1],
-			[1,1,1,1,1,1,1,1,1]]
-
-board2 = [	[1,1,1,1,1,1,1,1,1],
-			[1,0,0,0,0,0,0,0,1],
-			[1,0,1,0,1,0,0,0,1],
-			[1,0,0,0,0,0,0,0,1],
-			[1,0,1,0,1,0,1,0,1],
 			[1,0,0,0,0,0,0,0,1],
 			[1,1,1,1,1,1,1,1,1]]
 
@@ -159,13 +174,6 @@ robot = (1,1)
 target = [(4,4)]
 cans = [(2,4)]
 
-a = State(cans, robot)
-printStateFancy(a)
-b = generate_new_states(a)
-for i in b:
-	print (i._robot)
-	printStateFancy(i)
-
-
-
-
+begining = State(cans,robot, -1)
+a = solve_sokoban(begining)
+recursive_print_traject(a, len(a) - 1)
