95f050f0b8b86538c2587bf5a7af1ed48470fb84
Marius Sommerfeld <m.96.sommerfeld@gmail.com>
2021-09-21 13:40:15 +0200

    its working!!!

---
+++ behavior/main.py
@@ -0,0 +1,136 @@
+#!/usr/bin/env pybricks-micropython
+from pybricks.hubs import EV3Brick
+from pybricks.ev3devices import Motor, ColorSensor
+from pybricks.parameters import Port, Stop, Color
+from pybricks.robotics import DriveBase
+from random import randrange
+#from pybricks.pupdevices import ColorDistanceSensor
+from pybricks.tools import wait
+import csv
+
+import time
+import math
+import random
+from sokobanSolver import State, SokobanSolver
+
+#grad = 237/90
+grad = 267/90
+advance = 1800/1060
+
+class moving_robot():
+	def __init__(self):
+		self.control = EV3Brick()
+		self.motor_a = Motor(Port.A)
+		self.motor_b = Motor(Port.B)
+		self.base = DriveBase(self.motor_a, self.motor_b, wheel_diameter=55.5, axle_track=104)
+
+		self.sensor_left = ColorSensor(Port.S1)
+		self.sensor_center = ColorSensor(Port.S2)
+		self.sensor_right = ColorSensor(Port.S3)
+
+		self.rot=1
+		self.speed = 150 # faster when not moving can
+
+		self.base.settings(self.speed, self.speed)#, turn_rate, turn_acceleration)
+
+	def turn_by(self, rad):
+		self.base.turn(rad)
+
+	def move_forward(self, distance):
+		self.base.straight(-distance*advance)
+
+	def turn(self, moove, previous):
+		if moove == previous:
+			return 0
+		if moove == "up":
+			if previous == "right":
+				return 90
+			if previous == "down":
+				return 180
+			return -90
+		if moove == "right":
+			if previous == "up":
+				return -90
+			if previous == "down":
+				return 90
+			return 180
+		if moove == "down":
+			if previous == "up":
+				return 180
+			if previous == "right":
+				return -90
+			return 90
+
+		if moove == "left":
+			if previous == "up":
+				return 90
+			if previous == "right":
+				return 180
+			return -90
+
+
+
+	def follow_line(self, backward):
+		#self.base.straight(50*backward)
+		while True:
+			#print('left: ' + str(self.sensor_left.color()))
+			#print('right: ' + str(self.sensor_right.color()))
+			while self.sensor_center.color() == Color.BLACK:
+				if self.sensor_left.color() == Color.BLACK:
+					return
+
+				if self.sensor_right.color() == Color.BLACK:
+					return
+
+				self.base.drive(backward * self.speed, 0)
+				#print('after')
+			self.find_line()
+
+	def naviguate(self, mooves, init_direction):
+		previous = init_direction
+		for moove in mooves:
+			print(moove)
+			if moove == 'can':
+				self.follow_line(-1)
+				self.base.straight(270)
+				self.follow_line(1)
+				self.base.straight(-200)
+
+			else:
+				angle_to_rotate = self.turn(moove, previous)
+				print(angle_to_rotate)
+				previous = moove
+				self.turn_by(angle_to_rotate*grad)
+				self.follow_line(-1)
+				self.base.straight(-270)
+
+	def find_line(self):
+		while self.sensor_center.color() != Color.BLACK:
+			self.base.drive(0, self.rot * self.speed)
+			if self.sensor_left.color() == Color.BLACK:
+				self.rot = 1
+			if self.sensor_right.color() == Color.BLACK:
+				self.rot = -1
+
+if __name__ == "__main__":
+	##### initialising robot
+	my_robot = moving_robot()
+
+	# initialisiing the maze
+	cans = [(1,2)]
+	robot = (3,0)
+	target = [(3,3)]
+	board = [[0,0,0,0],
+            [0,0,0,0],
+            [0,0,0,0],
+            [0,0,0,0]]
+
+	# solving the maze
+	init_state = State(cans,robot, -1)
+	solver = SokobanSolver(board, target, robot, cans)
+	solution = solver.solve_sokoban(init_state)
+	plan = solver.recursive_print_traject(solution, len(solution) - 1, -1)[:-1]
+	print(plan)
+
+	# run the plan
+	my_robot.naviguate(plan, 'up')
---
+++ behavior/sokobanSolver.py
@@ -0,0 +1,197 @@
+from copy import deepcopy
+import time
+
+class SokobanSolver:
+	def __init__(self, board, targets, robot, can) -> None:
+		self.board = board
+		self.targets = targets
+		self.robot = robot
+		self.can = can
+		self.x = len(self.board[0]) - 1
+		self.y = len(self.board) - 1
+
+	def generate_new_states(self, state, prev):
+		ret = []
+		if state._robot[0] > 0: #are we at border
+			if self.board[state._robot[0] - 1][state._robot[1]] == 0: #is there a wall
+				if (state._robot[0] - 1, state._robot[1]) in state._cans:  #is there a can
+					if state._robot[0] > 1 and self.board[state._robot[0] - 2][state._robot[1]] == 0 and (state._robot[0] - 2, state._robot[1]) not 	in state._cans: #is the can is at the border, or is there a wall or a can in front of her
+						new_cans = []
+						for i in state._cans:
+							if i != (state._robot[0] - 1, state._robot[1]):
+								new_cans.append(i)
+						new_cans.append((state._robot[0] - 2, state._robot[1]))
+						ret.append(State(new_cans, (state._robot[0] - 1, state._robot[1]),prev))
+				else:
+					ret.append(State(state._cans, (state._robot[0] - 1, state._robot[1]),prev))
+		if state._robot[0] < self.y:
+			if self.board[state._robot[0] + 1][state._robot[1]] == 0:
+				if (state._robot[0] + 1, state._robot[1]) in state._cans:
+					if state._robot[0] < self.y - 1 and self.board[state._robot[0] + 2][state._robot[1]] == 0 and (state._robot[0] + 2, state._robot[1]) 	not in state._cans:
+						new_cans = []
+						for i in state._cans:
+							if i != (state._robot[0] + 1, state._robot[1]):
+								new_cans.append(i)
+						new_cans.append((state._robot[0] + 2, state._robot[1]))
+						ret.append(State(new_cans, (state._robot[0] + 1, state._robot[1]),prev))
+				else:
+					ret.append(State(state._cans, (state._robot[0] + 1, state._robot[1]),prev))
+		if state._robot[1] < self.x:
+			if self.board[state._robot[0]][state._robot[1] + 1] == 0:
+				if (state._robot[0], state._robot[1] + 1) in state._cans:
+					if state._robot[1] < self.x - 1 and self.board[state._robot[0]][state._robot[1] + 2] == 0 and (state._robot[0], state._robot[1] + 2) 	not in state._cans:
+						new_cans = []
+						for i in state._cans:
+							if i != (state._robot[0], state._robot[1] + 1):
+								new_cans.append(i)
+						new_cans.append((state._robot[0], state._robot[1] + 2))
+						ret.append(State(new_cans, (state._robot[0], state._robot[1] + 1), prev))
+				else:
+					ret.append(State(state._cans, (state._robot[0], state._robot[1] + 1), prev))
+		if state._robot[1] > 0:
+			if self.board[state._robot[0]][state._robot[1] - 1] == 0:
+				if (state._robot[0], state._robot[1] - 1) in state._cans:
+					if state._robot[1] > 1 and self.board[state._robot[0]][state._robot[1] - 2] == 0 and (state._robot[0], state._robot[1] - 2) not 	in state._cans:
+						new_cans = []
+						for i in state._cans:
+							if i != (state._robot[0], state._robot[1] - 1):
+								new_cans.append(i)
+						new_cans.append((state._robot[0], state._robot[1] - 2))
+						ret.append(State(new_cans, (state._robot[0], state._robot[1] - 1), prev))
+				else:
+					ret.append(State(state._cans, (state._robot[0], state._robot[1] - 1), prev))
+		return ret
+
+
+	def check_stuck(self, cans):
+		for can in cans:
+			if can not in self.targets:
+				if not (can[0] == 0 or can[0] == self.y or can[1] == 0 or can[1] == self.x):
+					if self.board[can[0]][can[1] + 1] == 1 and self.board[can[0] + 1][can[1]] == 1:
+						return True
+					if self.board[can[0] + 1][can[1]] == 1 and self.board[can[0]][can[1] - 1] == 1:
+						return True
+					if self.board[can[0]][can[1] - 1] == 1 and self.board[can[0] - 1][can[1]] == 1:
+						return True
+					if self.board[can[0]][can[1] + 1] == 1 and self.board[can[0] - 1][can[1]] == 1:
+						return True
+		return False
+
+
+	def solve_sokoban(self, begining):
+		self.targets.sort()
+		states_list = [begining]
+		begining = 0
+		end = 1
+		while (True):
+			a = 0
+			for i in range(begining, end):
+				new = self.generate_new_states(states_list[i], i)
+				for j in new:
+					if j not in states_list and self.check_stuck(j._cans) == False:
+						#printStateFancy(j)
+						states_list.append(j)
+						j._cans.sort()
+						a += 1
+						if self.targets == j._cans:
+							return states_list
+			begining = end
+			end += a
+			if a == 0:
+				return states_list
+
+	def recursive_print_traject(self, l, i, after):
+		if l[i]._prev != -1:
+			ret = self.recursive_print_traject(l, l[i]._prev, i)
+		else:
+			ret = []
+		#printStateFancy(l[i])
+		if l[after]._robot[0] > l[i]._robot[0]:
+			ret.append("down")
+		elif l[after]._robot[0] < l[i]._robot[0]:
+			ret.append("up")
+		elif l[after]._robot[1] > l[i]._robot[1]:
+			ret.append("right")
+		else:
+			ret.append("left")
+		if l[after]._cans != l[i]._cans:
+			ret.append("can")
+		return ret
+
+
+	def printStateFancy(self, state):
+		for i in range(len(self.board)):
+			line = ""
+			for j in range(len(self.board[i])):
+				e = self.board[i][j]
+				if(e == 0): e = " "	  #Empty
+				if(e == 1): e = "⊠"  #Walls
+				for t in self.targets:
+					if(t == (i,j)): e = "✛" #Target
+				for c in state._cans:
+					if(c == (i,j)): e = "⬤" #Can
+				if ((state._robot[0] , state._robot[1]) == (i,j)): e = "△" #Can
+				line = line + str(e) + " "
+			print(line)
+
+#	def check(list1, list2):
+#	for i in list1:
+#		if i in list2:
+#			list1.remove(i)
+#			list2.remove(i)
+#		else:
+#			return False
+#	return True
+
+class State:
+	def __init__(self, cans, robot, prev: int):
+		self._cans = cans
+		self._robot = robot
+		self._prev = prev
+
+	def __eq__(self, x):
+		if self._cans == x._cans and self._robot == x._robot:
+			return True
+		return False
+
+	def __str__(self):
+		a = ""
+		a += str(self._robot)
+		for i in self._cans:
+			a +=str(i)
+		return a
+
+#def board_translation(board):
+#	robot = -1
+#	cans = []
+#	target = []
+#	new_board = []
+#	for i in range(len(board)):
+#		new_line = []
+#		for j in range(len(board[i])):
+#			if board[i][j] == 'X':
+#				new_line.append(1)
+#			elif board[i][j] == '@':
+#				new_line.append(0)
+#				robot = i * len(board[i]) + j
+#			elif board[i][j] == '.':
+#				new_line.append(0)
+#				target.append((i,j))
+#			elif board[i][j] == '*':
+#				new_line.append(0)
+#				target.append((i,j))
+#				cans.append((i,j))
+#			elif board[i][j] == '$':
+#				new_line.append(0)
+#				can.append((i,j))
+#			elif board[i][j] == ' ':
+#				new_line.append(0)
+#			else:
+#				print ("ERRORRRRRRRRRR")
+#	return robot, can, target, new_board
+
+
+
+
+
+
