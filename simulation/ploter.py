fb836228d619620f891d25cbb67c17681baced34
a <a>
2021-10-05 14:02:11 +0200

    ploter ended

--- simulation/ploter.py
+++ simulation/ploter.py
@@ -1,25 +1,23 @@
 import matplotlib.pyplot as plt
 from math import cos, sin
+from time import sleep
 
 file1 = open("trajectory.dat", "r")
 lines = file1.readlines()
-cornersx = [-1, -1, 1, 1]
-cornersy = [-1, 1, -1, 1]
-x = []
-y = []
-dx = []
-dy = []
+cornersx = [-1.1, -1.1, 1.1, 1.1]
+cornersy = [-1.1, 1.1, -1.1, 1.1]
 
-step = [0]
-angle = []
 i = 0
-for line in lines:
-    if i % 5 == 0:
-        a, b, angle = line.split(",")
-        plt.arrow(float(a),float(b), 0.5 * cos(float(angle)), 0.5 * sin(float(angle)), width=0.01)
-    i += 1
 
-plt.scatter(cornersx,cornersy)
+for line in lines:
+	if i % 5 == 0:
+		a, b, angle = line.split(",")
+		plt.cla()
+		plt.arrow(float(a),float(b), 0.4 * cos(float(angle)), 0.4 * sin(float(angle)), width=0.01)
+		plt.scatter(cornersx,cornersy, 0)
+		plt.pause(0.01)
+		
+	i += 1
+
 plt.show()
-plt.savefig("test.png")
 
