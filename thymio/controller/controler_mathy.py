74d0b20248866539bb18201042815a42ddb77c54
a <a>
2021-10-18 12:41:16 +0200

    some changes

--- thymio/controller/controler_mathy.py
+++ thymio/controller/controler_mathy.py
@@ -1,27 +1,45 @@
 from kinematic_simulator import kinematic_simulator
 from pathfinding import pathfinder
 from random import randint
+import matplotlib.pyplot as plt
 
+
+def detect_local_min(l):
+	ret = []
+	s = len(l) + 1
+	ss = s - 2
+	for i in range(0, s):
+		if l[(i - 1) % ss] > l[i % ss] and l[i % ss] < l[(i + 1) % ss]:
+			ret.append(i % ss)
+	return ret
+	
 def generate_world():
 	h = 1
-	w = 1
+	w = 2
 	walls = [[-w/2, -w/2, -h/2, h/2], [w/2, w/2, -h/2,h/2],[-w/2,w/2,h/2,h/2],[-w/2,w/2,-h/2,-h/2]]
-	q = 0
-	x = 0
-	y = 0
+	q = randint(0,360) / 360
+	x = randint(-9,9) / 20
+	y = randint(-4,4) / 10
 	simulator = kinematic_simulator(walls)
 	return simulator, x,y,q
 
 if __name__ == "__main__":
 	simulator, x ,y ,q = generate_world()
 	coo = [[x, y ,q]]
-	while(len(coo) < 10000):
-		distance = simulator.thymio_sensor(coo[-1][0],coo[-1][1],coo[-1][2])
-		if distance[0] < 0.14 or distance[1] < 0.14 or distance[2] < 0.14 or distance[3] < 0.14 or distance[4] < 0.14:
-			if distance[0] + distance[1] != distance[3] + distance[4]:
-				coo.extend(simulator.simulate(coo[-1][0],coo[-1][1],coo[-1][2],2,-1,6))
-			else:
-				coo.extend(simulator.simulate(coo[-1][0],coo[-1][1],coo[-1][2],2, -1,3))
-		else:
-			coo.extend(simulator.simulate(coo[-1][0],coo[-1][1],coo[-1][2],1,1,1))
+	while(detect_local_min(simulator.lidar_sensor(coo[-1][0], coo[-1][1], coo[-1][2]))[0] != 1):
+		coo.extend(simulator.simulate(coo[-1][0],coo[-1][1],coo[-1][2],2,-1,0.1))
+	while(simulator.lidar_sensor(coo[-1][0], coo[-1][1], coo[-1][2])[0] > 0.1):
+		coo.extend(simulator.simulate(coo[-1][0],coo[-1][1],coo[-1][2],2,2,0.1))
+	coo.extend(simulator.simulate(coo[-1][0],coo[-1][1],coo[-1][2],-1,2,0.3))
+	while(detect_local_min(simulator.lidar_sensor(coo[-1][0], coo[-1][1], coo[-1][2]))[0] > 4):
+		coo.extend(simulator.simulate(coo[-1][0],coo[-1][1],coo[-1][2],-1,2,0.1))
+	while(simulator.lidar_sensor(coo[-1][0], coo[-1][1], coo[-1][2])[0] > 0.1):
+		coo.extend(simulator.simulate(coo[-1][0],coo[-1][1],coo[-1][2],2,2,0.1))
+	coo.extend(simulator.simulate(coo[-1][0],coo[-1][1],coo[-1][2],-1,2,0.3))
+	while(detect_local_min(simulator.lidar_sensor(coo[-1][0], coo[-1][1], coo[-1][2]))[0] > 4):
+		coo.extend(simulator.simulate(coo[-1][0],coo[-1][1],coo[-1][2],-1,2,0.1))
+	coo.extend(simulator.simulate(coo[-1][0],coo[-1][1],coo[-1][2],-1,2,0.3))
+	while(detect_local_min(simulator.lidar_sensor(coo[-1][0], coo[-1][1], coo[-1][2]))[0] > 4):
+		coo.extend(simulator.simulate(coo[-1][0],coo[-1][1],coo[-1][2],-1,2,0.1))
+
 	simulator.save(coo)
