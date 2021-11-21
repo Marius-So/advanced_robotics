8e0b2192fa4b6ffc6bb79926457d16e3808b4aa5
murf12 <murf12>
2021-11-21 15:09:02 +0000

    update

--- final_exam/Thymio.py
+++ final_exam/Thymio.py
@@ -5,7 +5,17 @@ import dbus
 import dbus.mainloop.glib
 import sys
 import os
+import time
+import sys
 
+thymio_colour = {
+    'red': [32,0,0],
+    'orange': [32,20,0],
+    'blue': [0,0,32],
+    'green': [0,32,0],
+    'purple': [16,0,16],
+    'off': [0,0,0]
+}
 
 class Thymio(object):
     def __init__(self, filename='thympi.aesl'):
@@ -35,12 +45,9 @@ class Thymio(object):
 
         # initialize some variables which can be used in the main loop
         # to set thymio states
-        self.l_speed = 0
-        self.r_speed = 0
-
         self.left_speed = 0
         self.right_speed = 0
-        self.acc = 0
+        self.rx = -1
 
         self.prox_horizontal = 0
         self.ground_ambiant = 0
@@ -48,28 +55,50 @@ class Thymio(object):
         self.ground_delta = 0
         self.run_on = True
 
+        self.turn_off_leds()
+        self.asebaNetwork.SendEventName( "prox.comm.enable", [1])
+        # TODO: check for error when using  -> prox.comm.
+        # self.asebaNetwork.SendEventName("prox.comm.tx",[0])
+
+    def __del__(self):
+        self.stop()
+        self.set_speed(0,0)
+        self.set_colour('off')
+        os.system("pkill -n asebamedulla")
+        sys.exit()
+
+
+    def send_event(self, name, inp):
+        self.asebaNetwork.SendEventName(
+            name,
+            inp,
+            reply_handler=self.dbusReply,
+            error_handler=self.dbusError
+        )
+
+    def turn_off_leds(self):
+        self.send_event('leds.prox.v',[0,0])
+        self.send_event('leds.buttons',[0,0,0,0])
+        self.send_event('leds.prox.h',[0,0,0,0,0,0,0])
+        self.send_event('leds.circle',[0,0,0,0,0,0,0])
+        self.send_event('leds.rc',[0])
+        self.send_event('leds.temperature',[0,0])
 
     def set_speed(self, l_speed, r_speed):
-        self.l_speed = l_speed
-        self.r_speed = r_speed
         self.asebaNetwork.SendEventName(
             'motor.target',
-            [self.l_speed, self.r_speed],
+            [l_speed, r_speed],
             reply_handler=self.dbusReply,
             error_handler=self.dbusError
         )
 
     def play_tune(self):
-        self.asebaNetwork.SendEventName("sound.play(1)")
-
-    def stopAsebamedulla(self):
-        # stop the asebamedulla process
-        # !!dbus connection will fail after this call
-        os.system("pkill -n asebamedulla")
+        self.asebaNetwork.SendEventName("sound.play",[3])
 
     def run(self):
         # run event loop all 20ms
-        threading.Timer(0.02, self.mainLoop).start()
+        if self.run_on:
+            threading.Timer(0.02, self.mainLoop).start()
 
     def dbusReply(self):
         # dbus replys can be handled here.
@@ -90,40 +119,35 @@ class Thymio(object):
         self.ground_delta = self.asebaNetwork.GetVariable('thymio-II', 'prox.ground.delta')
         self.left_speed = self.asebaNetwork.GetVariable("thymio-II", "motor.left.speed")
         self.right_speed = self.asebaNetwork.GetVariable("thymio-II", "motor.right.speed")
-        # print the readed sensor values
 
-        # increase the counter
+        # sending and receiving information
+
+        self.asebaNetwork.SendEventName("prox.comm.tx", [0])
+        self.rx = self.asebaNetwork.GetVariable("thymio-II", "prox.comm.rx")
         # reschedule mainLoop
         if self.run_on:
             self.run()
 
-    def set_colour(self):
+    def set_colour(self, colour):
+        self.send_event('leds.bottom.left',thymio_colour[colour])
+        self.send_event('leds.bottom.right',thymio_colour[colour])
+        self.send_event('leds.top',thymio_colour[colour])
 
-        self.asebaNetwork.SendEventName(
-            'leds.bottom.left',
-            [0,0,31],
-            reply_handler=self.dbusReply,
-            error_handler=self.dbusError
-        )
-        self.asebaNetwork.SendEventName(
-            'leds.bottom.right',
-            [0,0,31],
-            reply_handler=self.dbusReply,
-            error_handler=self.dbusError
-        )
-
+    def stop(self):
+        self.run_on = False
 
 def main():
     # check command-line arguments
-    #if len(sys.argv) != 2:
-    #    print('Usage %s FILE' % sys.argv[0])
-    #    return
-
     # create and run controller
     thymioController = Thymio()
     thymioController.run()
-    thymioController.set_colour()
-    thymioController.set_speed(50,50)
+    thymioController.set_colour('purple')
+    thymioController.set_speed(0,0)
+    for colour in ['red','orange','blue','green','purple']:
+        time.sleep(3)
+        print(colour)
+        thymioController.set_colour(colour)
+        thymioController.set_speed(0,0)
 
 if __name__ == '__main__':
     main()
--- final_exam/camera_analysis.py
+++ final_exam/camera_analysis.py
@@ -1,9 +1,20 @@
+import matplotlib.pyplot as plt
+
 def test_color(img,x, y):
     for i in range(y):
         for j in range(x):
-            if img[i][j][1] >= 1.5*img[i][j][0] and img[i][j][1] >= 1.5*img[i][j][2] and img[i][j][1] > 100:
-                print("green", i, j)
-            elif img[i][j][0] >= 1.5*img[i][j][1] and img[i][j][0] >= 1.5 *img[i][j][2] and img[i][j][0] > 100:
-                print("red", i,j)
-            elif img[i][j][2] >= 1.5*img[i][j][0] and img[i][j][2] >= 1.5*img[i][j][1] and img[i][j][2] > 100:
-                print("blue",i,j)
+            img[i][j] = [0,0,0]
+            if img[i][j][1] >= 1.5*img[i][j][0] and img[i][j][1] >= 1.5*img[i][j][2] and img[i][j][1] > 99:
+                img[i][j] == [0,255,0]
+            elif img[i][j][0] >= 2*img[i][j][1] and img[i][j][1] >= 2 * img[i][j][2]:
+                img[i][j] = [255,130, 0]
+            elif img[i][j][0] >= 1.5*img[i][j][1] and img[i][j][0] >= 1.5 *img[i][j][2] and img[i][j][0] > 99:
+                img[i][j] = [255,0,0]
+            elif img[i][j][2] >= 1.5*img[i][j][0] and img[i][j][2] >= 1.5*img[i][j][1] and img[i][j][2] > 99:
+                img[i][j] = [0, 0,255]
+            elif img[i][j][2] >= 1.5*img[i][j][1] and img[i][j][0] >= 1.5*img[i][j][1] and img[i][j][2] > 99:
+                img[i][j] = [230,230,250]
+            else:
+                img[i][j] = [0,0,0]
+    plt.imshow(img)
+    plt.show()
