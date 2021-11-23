095db3e2c53e56dd802611404662ffc7a0d1901c
nhbess <nbes@itu.dk>
2021-11-23 16:39:57 +0100

    Test for comunications

--- final_exam/hardware.py
+++ final_exam/hardware.py
@@ -9,7 +9,16 @@ class input_output(Thymio, camera, lidar):
         lidar.__init__(self)
         Thymio.__init__(self)
 
+def test_Comunications(io, msg):
+    #This enables the prox.comm rx value to zero, gets overwritten when receiving a value    
+    while True:
+        send = io.send_code(msg)
+        received = io.get_sensor_values()[4]
+        print("received: " + str(received))
+
+
 if __name__ == '__main__':
     io = input_output()
     sleep(1)
-    print(io.lidar_output)
+    #print(io.lidar_output)
+    test_Comunications(io,1)
