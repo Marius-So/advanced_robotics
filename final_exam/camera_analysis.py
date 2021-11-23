4139dbe7918b379e0eff8bada40eb9ff83493da6
murf12 <murf12>
2021-11-23 11:24:10 +0000

    newest version

--- final_exam/camera_analysis.py
+++ final_exam/camera_analysis.py
@@ -1,20 +1,59 @@
 import matplotlib.pyplot as plt
+import cv2
+import numpy as np
 
-def test_color(img,x, y):
-    for i in range(y):
-        for j in range(x):
-            img[i][j] = [0,0,0]
-            if img[i][j][1] >= 1.5*img[i][j][0] and img[i][j][1] >= 1.5*img[i][j][2] and img[i][j][1] > 99:
-                img[i][j] == [0,255,0]
-            elif img[i][j][0] >= 2*img[i][j][1] and img[i][j][1] >= 2 * img[i][j][2]:
-                img[i][j] = [255,130, 0]
-            elif img[i][j][0] >= 1.5*img[i][j][1] and img[i][j][0] >= 1.5 *img[i][j][2] and img[i][j][0] > 99:
-                img[i][j] = [255,0,0]
-            elif img[i][j][2] >= 1.5*img[i][j][0] and img[i][j][2] >= 1.5*img[i][j][1] and img[i][j][2] > 99:
-                img[i][j] = [0, 0,255]
-            elif img[i][j][2] >= 1.5*img[i][j][1] and img[i][j][0] >= 1.5*img[i][j][1] and img[i][j][2] > 99:
-                img[i][j] = [230,230,250]
-            else:
-                img[i][j] = [0,0,0]
-    plt.imshow(img)
-    plt.show()
+def analyse_for_colours(image, k_size = 8):
+    input_hsv = cv2.cvtColor(image, cv2.COLOR_RGB2HSV)
+    kernel = np.ones((k_size, k_size), np.uint8)
+
+    # lower mask (0-10)
+    lower_red = np.array([0,50,50])
+    upper_red = np.array([10,255,255])
+    mask0 = cv2.inRange(input_hsv, lower_red, upper_red)
+
+    # upper mask (170-180)
+    lower_red = np.array([170,50,50])
+    upper_red = np.array([180,255,255])
+    mask1 = cv2.inRange(input_hsv, lower_red, upper_red)
+
+    # join my masks
+    red_mask = mask0+mask1
+    red_mask = cv2.threshold(red_mask, 1, 255, cv2.THRESH_BINARY)[1]
+    red_mask = cv2.morphologyEx(red_mask, cv2.MORPH_OPEN, kernel)
+
+    # blue
+    lower_blue = np.array([110,50,50])
+    upper_blue = np.array([130,255,255])
+    blue_mask = cv2.inRange(input_hsv, lower_blue, upper_blue)
+    blue_mask = cv2.threshold(blue_mask, 1, 255, cv2.THRESH_BINARY)[1]
+    blue_mask = cv2.morphologyEx(blue_mask, cv2.MORPH_OPEN, kernel)
+
+    # green
+    lower_green = np.array([50,50,50])
+    upper_green = np.array([70,255,255])
+    green_mask = cv2.inRange(input_hsv, lower_green, upper_green)
+    green_mask = cv2.threshold(green_mask, 1, 255, cv2.THRESH_BINARY)[1]
+    green_mask = cv2.morphologyEx(green_mask, cv2.MORPH_OPEN, kernel)
+
+    # yellow
+    lower_yellow = np.array([25,50,50])
+    upper_yellow = np.array([35,255,255])
+    yellow_mask = cv2.inRange(input_hsv, lower_yellow, upper_yellow)
+    yellow_mask = cv2.threshold(yellow_mask, 1, 255, cv2.THRESH_BINARY)[1]
+    yellow_mask = cv2.morphologyEx(yellow_mask, cv2.MORPH_OPEN, kernel)
+
+    return red_mask, blue_mask, green_mask, yellow_mask
+
+def update_centers(self):
+    for mask in self.masks:
+        M = cv2.moments(self.masks[mask])
+        if not round(M["m10"]):
+            self.centers[mask] = int(M["m10"] / M["m00"]).self.masks[mask].shape[0]
+        else:
+            self.centers[mask] = None
+
+
+
+
+
+
--- final_exam/hardware.py
+++ final_exam/hardware.py
@@ -1,62 +1,15 @@
-from picamera import PiCamera
-from threading import Thread
-from time import sleep, time
-import numpy as np
-from math import floor
-from adafruit_rplidar import RPLidar, RPLidarException
+from camera import camera
+from lidar import lidar
+from Thymio import Thymio
+from time import sleep
 
-class input_output():
+class input_output(Thymio, camera, lidar):
     def __init__(self):
-        #camera
-        self.camera = PiCamera()
-        self.camera.start_preview()
-        sleep(0.1)
-        self.camera.resolution = (320, 240)
-        self.camera.framerate = 24
-        self.picture = np.empty((240, 320, 3), dtype=np.uint8)
-        self.fliped = self.picture
-        camera_thread = Thread(target=self.camera_sensing)
-        camera_thread_daemon = True
-        camera_thread.start()
+        camera.__init__(self)
+        lidar.__init__(self)
+        Thymio.__init__(self)
 
-        #lidar
-        self.lidar = RPLidar(None, '/dev/ttyUSB0')
-        self.lidar_output = {}
-        lidar_thread = Thread(target=self.lidar_sensing)
-        lidar_thread_daemon = True
-        lidar_thread.start()
-
-    def camera_sensing(self):
-        while True:
-            self.camera.capture(self.picture, 'rgb')
-            #self.fliped = np.flip(self.picture, 0)
-
-    def lidar_sensing(self):
-        while True:
-            try:
-                for scan in self.lidar.iter_scans():
-                    for (__,angle, distance) in scan:
-                        self.lidar_output[floor(angle)] = distance, time()
-            except RPLidarException:
-                self.lidar.stop()
-                self.lidar = RPLidar(None, '/dev/ttyUSB0')
-
-    def __del__(self):
-        #camera
-        self.camera.stop_preview()
-        self.camera.close()
-        #lidar
-        self.lidar.stop()
-        print("test")
-
-
-if __name__ == "__main__":
-    a = input_output()
-    sleep(0.1)
-    import matplotlib.pyplot as plt
-    from camera_analysis import test_color
-    while True:
-        a.fliped = np.flip(a.picture, 0)
-        test_color(a.fliped,320,240)
-        plt.imshow(a.fliped)
-        plt.show()
+if __name__ == '__main__':
+    io = input_output()
+    sleep(1)
+    print(io.lidar_output)
