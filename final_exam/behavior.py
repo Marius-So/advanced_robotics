722ec9bf1cc1bb807805f977105bc81e945b34f3
nhbess <nbes@itu.dk>
2021-11-26 15:29:10 +0100

    behaviour

--- final_exam/behavior.py
+++ final_exam/behavior.py
@@ -1,2 +1,14 @@
 #simple one
+import time
+from hardware import input_output
+from camera_analysis import analyse_for_colours, get_bin_detection
+import matplotlib as plt
 
+if __name__ == '__main__':
+    io = input_output()
+    pic = io.take_picture()
+    plt.imshow(pic)
+    plt.show()
+    
+    #print(io.lidar_output)
+    #test_Comunications(io,1)
--- final_exam/hardware.py
+++ final_exam/hardware.py
@@ -10,31 +10,3 @@ class input_output(Thymio, camera, lidar):
         camera.__init__(self)
         lidar.__init__(self)
         Thymio.__init__(self)
-
-def test_Comunications(io, msg):
-    #This enables the prox.comm rx value to zero, gets overwritten when receiving a value
-    while True:
-        send = io.send_code(msg)
-        received = io.get_sensor_values()[4]
-        print("received: " + str(received))
-
-
-if __name__ == '__main__':
-    from camera_analysis import mask
-    import matplotlib.pyplot as plt
-    from time import sleep
-
-    io = input_output()
-    sleep(1)
-    picture = io.take_picture()
-    start = time.time()
-    mask(picture)
-    print(time.time()- start)
-    start = time.time()
-    y = analyse_for_colours(picture)
-    print(time.time()- start)
-
-    plt.imshow(np.concatenate(y))
-    plt.show()
-    #print(io.lidar_output)
-    #test_Comunications(io,1)
