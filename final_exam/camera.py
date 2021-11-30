9189a39ec5abdfa97d3ec92fb6ef10b4f56c6962
a <a>
2021-11-30 11:40:49 +0100

    camera resolution

--- final_exam/camera.py
+++ final_exam/camera.py
@@ -8,9 +8,9 @@ class camera():
         self.camera = PiCamera()
         self.camera.start_preview()
         sleep(0.1)
-        self.camera.resolution = (128, 96)
+        self.camera.resolution = (216, 162)
         self.camera.framerate = 24
-        self.picture = np.empty((96, 128, 3), dtype=np.uint8)
+        self.picture = np.empty((162, 216, 3), dtype=np.uint8)
 
     def take_picture(self):
         self.camera.capture(self.picture, 'rgb')
