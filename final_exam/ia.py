f2dc62137db6fbb1801dcd9a092faeb047207e8f
a <a>
2021-11-30 13:32:24 +0100

    input builder for ai

--- final_exam/ia.py
+++ final_exam/ia.py
@@ -1,5 +1,23 @@
+from time import time
+from math import inf
+
 class ia:
 	def __init__(self):
 
 	
-	def build_input(self, lidar_output, camera_output):
+	def build_input(self, lidar_output, camera_output, ds=10):
+		output = []
+		for i in range(0, 360, ds):
+			for j in range(i, i + ds):
+				m = inf
+				if time() - lidar_output[(j + 180)%360][2] < 1 and lidar_output[(j + 180)%360][1] < m:
+					m = lidar_output[(j + 180)%360][1]
+			if m == inf:
+				output.append(0)
+			else:
+				output.append(1 - m/1000)
+		for i in camera_output:
+			for j in i:
+				output.append(j)
+		return output
+
